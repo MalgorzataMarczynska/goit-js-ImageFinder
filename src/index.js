76912d1970c6c90ebf1f266c38b7763d48e85bb3
MalgorzataMarczynska <malgorzatamarczynska83@gmail.com>
2022-12-11 21:40:02 +0100

    scroll update

--- src/index.js
+++ src/index.js
@@ -123,3 +123,11 @@ const lightboxGallery = () => {
     lightboxGallery.refresh();
   });
 };
+const { height: cardHeight } = document
+  .querySelector('.gallery')
+  .firstElementChild.getBoundingClientRect();
+
+window.scrollBy({
+  top: cardHeight * 2,
+  behavior: 'smooth',
+});
